b4c623346dccd2c2bfb892c5b20478178372bf8c
Andre Sieiro <andresieiro@hotmail.com>
2017-10-18 18:01:35 -0200

    fixes

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 Examples:
 
-create:
+Commands:
+
+create
   react-cli create MyComponent react-component  
   (generates a state component on ./src/components/MyComponent/index.js)
 
+  react-cli create MyComponent react-component -s
   react-cli create MyComponent react-component -stateless   
   (generates a stateless component on ./src/components/MyComponent/index.js)
--- help/index.js
+++ help/index.js
@@ -6,6 +6,7 @@ var help = function() {
   console.log('');
   console.log(' create:');
   console.log(colors.yellow('   react-cli create MyComponent react-component') + '   (generates a state component on ./src/components/MyComponent/index.js) ')
+  console.log(colors.yellow('   react-cli create MyComponent react-component -s');
   console.log(colors.yellow('   react-cli create MyComponent react-component -stateless') + '   (generates a stateless component on ./src/components/MyComponent/index.js) ')
   console.log('');
 }
